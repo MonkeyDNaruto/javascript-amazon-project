fe198482d6df4bdf4fa2b0bfabe8fc6924e8c5c7
Nischal Shrestha <nischalshrestha324@gmail.com>
2025-09-22 19:04:26 +0545

    delete product from cart when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,7 +15,7 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHtml += `
-          <div class="cart-item-container">
+          <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Wednesday, June 15
             </div>
@@ -97,5 +97,8 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener('click', () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
+
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
   })
 })
