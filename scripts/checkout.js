7c83f0b5bb84beb3ee102841ebe5dd1f3b4850f9
Nischal Shrestha <nischalshrestha324@gmail.com>
2026-01-23 19:53:48 +0545

    make code better

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymantSummary.js";
+import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 
+renderCheckoutHeader();
 renderOrderSummary();
 renderPaymentSummary();
