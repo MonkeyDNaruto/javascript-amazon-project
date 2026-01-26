1e0b434e6ae9b45ca95b2ae73ac73ef14efa1f35
Nischal Shrestha <nischalshrestha324@gmail.com>
2026-01-26 19:57:54 +0545

    create automated test for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,47 +1,55 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if(!cart) {
-  cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-    deliveryOptionId: '1'
-  }, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId: '2'
-  }];
+loadFromStroage();
+
+export function loadFromStroage() {
+  cart = JSON.parse(localStorage.getItem("cart"));
+
+  if (!cart) {
+    cart = [
+      {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+        deliveryOptionId: "1",
+      },
+      {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+        deliveryOptionId: "2",
+      },
+    ];
+  }
 }
 
-
 function saveToStorage() {
-  localStorage.setItem('cart', JSON.stringify(cart));
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
 
 export function addToCart(productId) {
   let matchingItem;
 
   cart.forEach((cartItem) => {
-    if(productId === cartItem.productId) {
+    if (productId === cartItem.productId) {
       matchingItem = cartItem;
     }
-  })
+  });
 
-  if(matchingItem) {
+  if (matchingItem) {
     matchingItem.quantity++;
   } else {
     cart.push({
       productId: productId,
       quantity: 1,
-      deliveryOptionId: '1'
+      deliveryOptionId: "1",
     });
   }
   saveToStorage();
-};
+}
 
 export function removeFromCart(productId) {
   let newCart = [];
   cart.forEach((cartItem) => {
-    if(cartItem.productId !== productId) {
+    if (cartItem.productId !== productId) {
       newCart.push(cartItem);
     }
   });
@@ -53,10 +61,10 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
   let matchingItem;
 
   cart.forEach((cartItem) => {
-    if(productId === cartItem.productId) {
+    if (productId === cartItem.productId) {
       matchingItem = cartItem;
     }
-  })
+  });
 
   matchingItem.deliveryOptionId = deliveryOptionId;
 
@@ -70,12 +78,12 @@ export function calculateCartQuantity() {
     cartQuantity += cartItem.quantity;
   });
   return cartQuantity;
-};
+}
 
 export function updateQuantity(productId, newQuantity) {
   let matchingItem;
   cart.forEach((cartItem) => {
-    if(cartItem.productId === productId) {
+    if (cartItem.productId === productId) {
       matchingItem = cartItem;
     }
   });
@@ -83,4 +91,4 @@ export function updateQuantity(productId, newQuantity) {
   matchingItem.quantity = newQuantity;
 
   saveToStorage();
-};
+}
--- tests-jasmine/utlis/moneyTest.js
+++ tests-jasmine/utlis/moneyTest.js
@@ -1,4 +1,4 @@
-import { formatCurrency } from "../scripts/utils/money.js";
+import { formatCurrency } from "../../scripts/utils/money.js";
 
 describe("test suite: formatCurreny", () => {
   it("convert cents into dollers", () => {
