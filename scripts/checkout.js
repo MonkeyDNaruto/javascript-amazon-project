2b4fc9c87057d9f4ee067195fc62c113e02cdacb
Nischal Shrestha <nischalshrestha324@gmail.com>
2026-01-29 14:39:21 +0545

    create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymantSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
+import "../data/cart-oop.js";
 
 renderCheckoutHeader();
 renderOrderSummary();
