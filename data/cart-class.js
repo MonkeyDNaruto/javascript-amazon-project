25f5f3cca1b6953a23f17e699d5abf9636095d21
Nischal Shrestha <nischalshrestha324@gmail.com>
2026-01-31 18:32:18 +0545

    make localStorageKey and loadFromStorage privete

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,15 +1,15 @@
 class Cart {
   cartItems;
-  localStorageKey;
+  #localStorageKey;
 
   constructor(localStorageKey) {
-    this.localStorageKey = localStorageKey;
+    this.#localStorageKey = localStorageKey;
 
-    this.loadFromStroage();
+    this.#loadFromStroage();
   }
 
-  loadFromStroage() {
-    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+  #loadFromStroage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
 
     if (!this.cartItems) {
       this.cartItems = [
@@ -28,7 +28,7 @@ class Cart {
   }
 
   saveToStorage() {
-    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
   }
 
   addToCart(productId) {
