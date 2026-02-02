4f4128a4d9b680a1825ae470ec467f27fbb508f1
Nischal Shrestha <nischalshrestha324@gmail.com>
2026-02-02 16:53:16 +0545

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,7 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymantSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 // import "../data/cart-class.js";
+import "../data/backend-practice.js"
 
 renderCheckoutHeader();
 renderOrderSummary();
