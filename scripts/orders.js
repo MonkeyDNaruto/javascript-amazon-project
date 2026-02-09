c3e1359b5e99f86c6baebf18503443d071e999de
Nischal Shrestha <nischalshrestha324@gmail.com>
2026-02-09 19:34:07 +0545

    make order page using js

--- scripts/orders.js
+++ scripts/orders.js
@@ -1,89 +1,124 @@
-function renderOrders() {
+import { calculateCartQuantity } from "../data/cart.js";
+import { orders } from "../data/orders.js";
+import { getProduct, loadProductsFetch } from "../data/products.js";
+import formatCurrency from "./utils/money.js";
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
+async function loadPage() {
+
+  await loadProductsFetch();
+
+  let ordersHTML = "";
+
+  orders.forEach((order) => {
+    const orderTimeString = dayjs(order.orderTime).format('MMMM DD')
+
+    ordersHTML += `
+    <div class="order-container">
+          
+      <div class="order-header">
+        <div class="order-header-left-section">
+          <div class="order-date">
+            <div class="order-header-label">Order Placed:</div>
+            <div>${orderTimeString}</div>
+          </div>
+          <div class="order-total">
+            <div class="order-header-label">Total:</div>
+            <div>$${formatCurrency(order.totalCostCents)}</div>
+          </div>
+        </div>
+
+        <div class="order-header-right-section">
+          <div class="order-header-label">Order ID:</div>
+          <div>${order.id}</div>
+        </div>
+      </div>
+
+      <div class="order-details-grid">
+        ${productsListHTML(order)}
+      </div>
+    </div>
+    `;
+
+    function productsListHTML(order) {
+      let productsListHTML = '';
+
+      order.products.forEach((productDetails) => {
+        const product = getProduct(productDetails.productId)
+
+        productsListHTML += `
+
+        <div class="product-image-container">
+          <img src="${product.image}">
+        </div>
+
+        <div class="product-details">
+          <div class="product-name">
+            ${product.name}
+          </div>
+          <div class="product-delivery-date">
+            Arriving on: ${ dayjs(productDetails.estimatedDeliveryTime).format('MMMM D')}
+          </div>
+          <div class="product-quantity">
+            Quantity: ${productDetails.quantity}
+          </div>
+          <button class="buy-again-button button-primary">
+            <img class="buy-again-icon" src="images/icons/buy-again.png">
+            <span class="buy-again-message">Buy it again</span>
+          </button>
+        </div>
+
+        <div class="product-actions">
+          <a href="tracking.html?orderId=${order.id}&productId=${product.id}">
+            <button class="track-package-button button-secondary">
+              Track package
+            </button>
+          </a>
+        </div>
+        `
+      })
+
+      return productsListHTML;
+    }
+  });
+
+  document.querySelector(".js-orders").innerHTML = ordersHTML;
+
+  function updateCartQuantity() {
+    document.querySelector(".js-cart-quantity").innerHTML =
+      calculateCartQuantity();
+  }
+
+  updateCartQuantity();
 }
 
-// import { products } from "./products";
+loadPage();
 
-// export const orders = JSON.parse(localStorage.getItem("orders")) || [];
 
-// export function addOrder(order) {
-//   orders.unshift(order);
-//   saveToStorage();
-// }
+// <div class="product-image-container">
+// <img src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg">
+// </div>
 
-// function saveToStorage() {
-//   localStorage.setItem("orders", JSON.stringify(orders));
-// }
+// <div class="product-details">
+// <div class="product-name">
+//   Adults Plain Cotton T-Shirt - 2 Pack
+// </div>
+// <div class="product-delivery-date">
+//   Arriving on: August 19
+// </div>
+// <div class="product-quantity">
+//   Quantity: 2
+// </div>
+// <button class="buy-again-button button-primary">
+//   <img class="buy-again-icon" src="images/icons/buy-again.png">
+//   <span class="buy-again-message">Buy it again</span>
+// </button>
+// </div>
 
-// export function renderOrders() {
-//   const container = document.querySelector(".js-orders");
-//   if (!container) return;
-
-//   let ordersHTML = "";
-
-//   orders.forEach((order) => {
-//     ordersHTML += `
-//       <div class="order-container">
-//         <div class="order-header">
-//           <div class="order-header-left-section">
-//             <div class="order-date">
-//               <div class="order-header-label">Order Placed:</div>
-//               <div>${new Date(order.orderTime).toDateString()}</div>
-//             </div>
-//             <div class="order-total">
-//               <div class="order-header-label">Total:</div>
-//               <div>$${(order.totalCostCents / 100).toFixed(2)}</div>
-//             </div>
-//           </div>
-
-//           <div class="order-header-right-section">
-//             <div class="order-header-label">Order ID:</div>
-//             <div>${order.id}</div>
-//           </div>
-//         </div>
-
-//         <div class="order-details-grid">
-//           ${order.products
-//             .map(
-//               (product) => `
-//                 <div class="product-image-container">
-//                   <img src="${product.image}">
-//                 </div>
-
-//                 <div class="product-details">
-//                   <div class="product-name">
-//                     ${product.name}
-//                   </div>
-//                   <div class="product-delivery-date">
-//                     Arriving on: ${new Date(
-//                       product.estimatedDeliveryTime
-//                     ).toDateString()}
-//                   </div>
-//                   <div class="product-quantity">
-//                     Quantity: ${product.quantity}
-//                   </div>
-
-//                   <button class="buy-again-button button-primary">
-//                     Buy it again
-//                   </button>
-//                 </div>
-
-//                 <div class="product-actions">
-//                   <a href="tracking.html">
-//                     <button class="track-package-button button-secondary">
-//                       Track package
-//                     </button>
-//                   </a>
-//                 </div>
-//               `
-//             )
-//             .join("")}
-//         </div>
-//       </div>
-//     `;
-//   });
-
-//   container.innerHTML = ordersHTML;
-// }
-
-// document.addEventListener("DOMContentLoaded", renderOrders);
+// <div class="product-actions">
+// <a href="tracking.html?orderId=123&productId=456">
+//   <button class="track-package-button button-secondary">
+//     Track package
+//   </button>
+// </a>
+// </div>
